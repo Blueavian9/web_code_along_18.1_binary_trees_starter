dc177e90cd25029e660cd5616a89de9b97857570
Cesar Aguilar <Blueavian9@gmail.com>
2023-10-10 17:00:17 -0700

    Initial commit

--- 1_bfs.js
+++ 1_bfs.js
@@ -2,45 +2,51 @@ class node {
   constructor(value) {
     this.value = value
     this.left = null
-    this.right = null
+    this.right = null 
   }
 }
 
 function traverse(root) {
-  // Implement
+
   const values = [];
-  const queue = [];
+  const queue = []; 
   queue.push(root)
-  
+
   while(queue.length != 0) {
-    // remove from queue
+
     const current = queue.shift();
 
-    // add value of removed node to values
-    values.push(current.value);
+    value.push(current.value);
 
-    // check if removed node has a left child, if yes, add to queue
     if(current.left != null) {
       queue.push(current.left);
     }
-    // check if removed node has a right child, if yes, add to queue
-    if(current.right != null) {
+    if (current.right != null) {
       queue.push(current.right);
     }
   }
-  // return value
-  return values;
+  return values; 
 }
 
+// Test cases: 
+const head = new node(6);
+head.left = new node(3);
+head.right = new node(9);
+
+console.log(traverse(head0) // 3, 6, 9
+
+/* 
+Plan: 
+1. 
+2. 
+3. 
+4.
 
 
 
-// Test cases:
-const head = new node(6)
-head.left = new node(3)
-head.right = new node(9)
 
-console.log(traverse(head)) // 6, 3, 9
+*/
+
 
 // Implement a level-order breadth-first traversal for a binary tree using the provided class for a binary tree node.
 // the return should be a list of values in the binary tree, based on a level-order (searching left to right at each "level") breadth-first traversal
